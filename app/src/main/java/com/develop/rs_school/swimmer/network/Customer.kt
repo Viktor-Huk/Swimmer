a9ab038eac6734f7bfa2e9008098706af5058024
LisPI <paschka.lis@gmail.com>
2020-08-01 13:34:17 +0300

    feat: implement customer api request

--- app/src/main/java/com/develop/rs_school/swimmer/network/Customer.kt
+++ app/src/main/java/com/develop/rs_school/swimmer/network/Customer.kt
@@ -1,7 +1,24 @@
 package com.develop.rs_school.swimmer.network
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Customer(val id : String) : Parcelable
+data class Customer(
+    val id: String,
+    val name: String,
+    val dob: String,
+    val balance: String,
+    @Json(name = "paid_count") val paid_lesson: Int
+) : Parcelable
+
+
+@Parcelize
+data class CustomerList(
+    val total: Int,
+    val items: List<Customer>
+) : Parcelable
+
+
+
