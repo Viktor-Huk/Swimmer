232408734a9e0d8f6ad2d00c357162c3df0c5782
ykhudaleyeu <egor1234@gmail.com>
2020-08-02 21:37:01 +0300

    fix logout button

--- app/src/main/java/com/develop/rs_school/swimmer/ProfileFragment.kt
+++ app/src/main/java/com/develop/rs_school/swimmer/ProfileFragment.kt
@@ -1,13 +1,16 @@
 package com.develop.rs_school.swimmer
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.fragment_profile.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class ProfileFragment : Fragment() {
 
@@ -26,5 +29,25 @@ class ProfileFragment : Fragment() {
         model.profile.observe(viewLifecycleOwner, Observer {
             tv_name.text = it.name
         })
+
+        view.settings.setOnClickListener {
+            openActivity()
+        }
+
     }
+
+    private fun openActivity() {
+        startActivity(
+            Intent(
+                context,
+                LoginActivity::class.java
+            ).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+
 }
