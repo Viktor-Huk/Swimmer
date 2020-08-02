2a1399e7e3d0edec6b2f75929a1135a6859bf1fa
ykhudaleyeu <egor1234@gmail.com>
2020-08-02 11:50:34 +0300

    change icon color + remove logout screen

--- app/src/main/java/com/develop/rs_school/swimmer/AccoutActivity.kt
+++ app/src/main/java/com/develop/rs_school/swimmer/AccoutActivity.kt
@@ -1,5 +1,6 @@
 package com.develop.rs_school.swimmer
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AlphaAnimation
@@ -9,7 +10,6 @@ import android.widget.Toast
 
 class AccoutActivity : BaseActivity(0){
 
-    lateinit var back: ImageButton
     lateinit var settingsButton : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,32 +19,12 @@ class AccoutActivity : BaseActivity(0){
         setContentView(R.layout.account_layout)
         Toast.makeText(this, "Account", Toast.LENGTH_SHORT).show()
 
-        back = findViewById<View>(R.id.backToMain) as ImageButton
         settingsButton = findViewById(R.id.settings) as ImageButton
 
-        back.setOnClickListener {
-            it.startAnimation(AlphaAnimation(1f, 0.8f))
-            onBackPressed()
-        }
 
         settingsButton.setOnClickListener {
-            it.startAnimation(AlphaAnimation(1f, 0.8f))
-            supportFragmentManager.beginTransaction().replace(android.R.id.content, SettingsFragment())
-                .commit()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    override fun onWindowFocusChanged(hasFocus: Boolean) {
-        super.onWindowFocusChanged(hasFocus)
-        if (hasFocus) hideSystemUI()
-    }
-
-    private fun hideSystemUI() {
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
