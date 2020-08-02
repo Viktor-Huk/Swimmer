dcea52af94711003ecbd019763cc539b86498a08
ykhudaleyeu <egor1234@gmail.com>
2020-08-02 09:42:29 +0300

    add adapter + add logout screen + add logout alert fragment

--- app/src/main/java/com/develop/rs_school/swimmer/HomeScreenActivity.kt
+++ app/src/main/java/com/develop/rs_school/swimmer/HomeScreenActivity.kt
@@ -2,12 +2,25 @@ package com.develop.rs_school.swimmer
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.toolbar_layout.*
 
-class HomeScreenActivity : BaseActivity(0){
+class HomeScreenActivity : BaseActivity(1){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.toolbar_layout)
         setupBottomNavigationBar()
+        initRecyclerView()
         Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+    }
+
+    lateinit var recyclerViewAdapter: RecyclerViewAdapter
+
+    private fun initRecyclerView() {
+        recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@HomeScreenActivity)
+            recyclerViewAdapter = RecyclerViewAdapter()
+            adapter = recyclerViewAdapter
+        }
     }
 }
