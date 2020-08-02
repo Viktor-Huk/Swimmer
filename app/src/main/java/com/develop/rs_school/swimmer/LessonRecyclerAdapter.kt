f2b84fe1c11bb92aabc570f41aa40ccc4235c439
ykhudaleyeu <egor1234@gmail.com>
2020-08-02 20:38:14 +0300

    update recycler view + add images

--- app/src/main/java/com/develop/rs_school/swimmer/LessonRecyclerAdapter.kt
+++ app/src/main/java/com/develop/rs_school/swimmer/LessonRecyclerAdapter.kt
@@ -4,6 +4,7 @@ package com.develop.rs_school.swimmer
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -33,15 +34,65 @@ class LessonRecyclerAdapter(private val itemClickListener: LessonRecyclerItemLis
             //itemBinding.name.text = customerLessonWithAgenda.date.toString()
 
             itemBinding.date.text = customerLessonWithAgenda.date?.toString()
+            itemBinding.weekDay.text = customerLessonWithAgenda.date.toString().split("\\s".toRegex())[0]
+            val month = customerLessonWithAgenda.date.toString().split("\\s".toRegex())[1]
+            val date = customerLessonWithAgenda.date.toString().split("\\s".toRegex())[2]
+            itemBinding.date.text = "$date $month"
 
             if(customerLessonWithAgenda.agendaStatus == AgendaStatus.MISSED_FREE){
-                //TODO ...
-                itemView.setBackgroundColor(Color.GREEN)
-                itemBinding.image.setImageResource(R.drawable.person_icon)
+                itemView.setBackgroundResource(R.drawable.layout_border_green)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_done_24)
             }
             if(customerLessonWithAgenda.agendaStatus == AgendaStatus.CANCELED){
-                //TODO ...
+                itemView.setBackgroundResource(R.drawable.layout_border_gray)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_remove_circle_outline_24)
             }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.PLANNED ){
+                itemView.setBackgroundResource(R.drawable.layout_border_gray)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.FORGOT){
+                itemBinding.icon.setImageResource(R.drawable.question)
+                itemView.setBackgroundResource(R.drawable.layout_border_gray)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.MISSED_NOT_PAID){
+                itemView.setBackgroundResource(R.drawable.layout_border_red)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_close_24)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.MISSED_PAID){
+                itemView.setBackgroundResource(R.drawable.layout_border_yellow)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_remove_circle_outline_24)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.PREPAID){
+                itemView.setBackgroundResource(R.drawable.layout_border_green)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.VISIT_PAID){
+                itemView.setBackgroundResource(R.drawable.layout_border_green)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_done_24)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+            }
+            if(customerLessonWithAgenda.agendaStatus == AgendaStatus.VISIT_NOT_PAID){
+                itemView.setBackgroundResource(R.drawable.layout_border_red)
+                itemBinding.icon.setImageResource(R.drawable.ic_baseline_close_24)
+                selectIconType(itemBinding, customerLessonWithAgenda.type)
+            }
+        }
+    }
+
+    private fun selectIconType(itemBinding: RecyclerViewRawBinding, status : String){
+        when(status){
+            "1" -> itemBinding.image.setImageResource(R.drawable.person_icon)
+            "2" -> itemBinding.image.setImageResource(R.drawable.person_stalker)
+            "3" -> itemBinding.image.setImageResource(R.drawable.asterisk)
+            "4" -> itemBinding.image.setImageResource(R.drawable.paperplane)
+            "5" -> itemBinding.image.setImageResource(R.drawable.md_contacts)
+            "6" -> itemBinding.image.setImageResource(R.drawable.asterisk)
         }
     }
 
